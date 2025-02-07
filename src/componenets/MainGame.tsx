3010149014434933589aeddcf48c50efc11bc031
sickk <jaakmyr@gmail.com>
2025-02-06 18:48:29 -0800

    wiat why is it lowkey not slop lslolololololol

--- src/componenets/MainGame.tsx
+++ src/componenets/MainGame.tsx
@@ -1,7 +1,52 @@
+import { useState, useRef } from "react"
 
-function MainGame() {
+enum STATES {
+    INIT,
+    GREEN,
+    WAITFORGREEN,
+    TOOSOON,
+
+}
+function MainGame() { 
+    const [state, setState] = useState(STATES.INIT)
+    const counter = useRef(0);
+
+    const handleClick = () => {
+        switch (state){
+            case STATES.INIT:
+                setState(STATES.WAITFORGREEN);
+                break;
+            case STATES.WAITFORGREEN:
+                if(counter.current > 0){ //if counting
+                    setState(STATES.GREEN)
+                }
+                else{
+                    setState(STATES.TOOSOON)
+                }
+                break;
+            case STATES.TOOSOON:
+                setState(STATES.WAITFORGREEN);
+                break;
+            default:
+                break;
+        }
+    }
+
   return (
-    <section id="maingame"></section>
+    <section onClick={handleClick} id="maingame">
+        {state === STATES.INIT && 
+        <div>init</div>
+        }
+        {state === STATES.GREEN && 
+        <div>GREEN</div>
+        }
+        {state === STATES.WAITFORGREEN && 
+        <div>WAITFORGREEN</div>
+        }
+        {state === STATES.TOOSOON && 
+        <div>TOOSOON</div>
+        }
+    </section>
   )
 }
 
